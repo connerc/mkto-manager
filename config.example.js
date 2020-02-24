6fdb52f11750f73591567aa69ea2cff49bbba30f
Conner C <conner.cloud@outlook.com>
2020-02-23 21:29:59 -0700

    Config example files and start of package loader index

---
+++ config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MK_ENDPOINT_URL: "https://576-NIT-050.mktorest.com",
+    MK_API_USERNAME: "email@email.com",
+    MK_API_CLIENT_ID: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
+    MK_API_CLIENT_SECRET: "iamaverylengthyandrandomsecretcreatedbymarketo",
+}
---
+++ config.test.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MK_ENDPOINT_URL: "http://localhost:8081",
+    MK_API_USERNAME: "email@email.com",
+    MK_API_CLIENT_ID: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
+    MK_API_CLIENT_SECRET: "iamaverylengthyandrandomsecretcreatedbymarketo",
+}
