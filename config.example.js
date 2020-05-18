3df0884f2826ede29f1582a1c12699dca5175291
connerc <conner.cloud@outlook.com>
2020-05-18 14:53:22 -0700

    Correcting asset pathing, config example, component-emitter, README

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MK_ENDPOINT_URL: "https://576-NIT-050.mktorest.com",
+    MK_ENDPOINT_URL: "https://123-ABC-456.mktorest.com",
     MK_API_USERNAME: "email@email.com",
     MK_API_CLIENT_ID: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
     MK_API_CLIENT_SECRET: "iamaverylengthyandrandomsecretcreatedbymarketo",
