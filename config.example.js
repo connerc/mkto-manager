80273b89b3a704af68cd0608261ba90aae8f4adb
connerc <conner.cloud@outlook.com>
2020-11-19 13:41:21 -0700

    IoC container build and handler wrapper

--- config.example.js
+++ config.example.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MK_ENDPOINT_URL: "https://123-ABC-456.mktorest.com",
-    MK_API_USERNAME: "email@email.com",
-    MK_API_CLIENT_ID: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
-    MK_API_CLIENT_SECRET: "iamaverylengthyandrandomsecretcreatedbymarketo",
+    mktoBaseUrl: "https://123-ABC-456.mktorest.com",
+    mktoClientUsername: "email@email.com",
+    mktoClientId: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
+    mktoClientSecret: "iamaverylengthyandrandomsecretcreatedbymarketo",
 }
