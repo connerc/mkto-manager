2a63e32bef5f114d05e1dfc29c27e4623d5080da
connerc <conner.cloud@outlook.com>
2021-06-18 11:07:12 -0700

    Nock server interceptor and initial test suite POC

---
+++ test/nock.js
@@ -0,0 +1,22 @@
+const nock = require("nock");
+const targetDomain = "http://localhost:8081";
+const mktoAccessToken = "mkto-test-access-token"
+const scope = nock(targetDomain)
+    .persist()
+	.get("/identity/oauth/token")
+	.query({    
+		grant_type: "client_credentials",
+		client_id: "GUID-GOES-HERE-FOR-THE-CLIENT-ID",
+		client_secret: "iamaverylengthyandrandomsecretcreatedbymarketo",
+	})
+	.reply(200, {
+		access_token: mktoAccessToken,
+		scope: "string",
+		expires_in: (new Date).getTime(),
+		token_type: "bearer",
+	});
+
+scope._targetDomain = targetDomain;
+scope._accessToken = mktoAccessToken;
+
+module.exports = scope;
